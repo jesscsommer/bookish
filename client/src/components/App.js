307b570c9ba8eaee3173c8f08d0b1e27fe760a8a
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 09:35:24 -0700

    Add sign up form, book card templates

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import AuthForm from "./account/AuthForm";
+import BookCard from "./books/BookCard";
 
 const App = () => {
   return (
     <div className="app">
-      <AuthForm />
+      {/* <AuthForm /> */}
+      <BookCard />
     </div>
   )
 }
