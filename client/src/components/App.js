24c6b82e4f56d261722a77e4c9a71ade5fcf27de
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 09:39:11 -0700

    Add header template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import AuthForm from "./account/AuthForm";
 import BookCard from "./books/BookCard";
+import Header from "./building_blocks/Header";
 
 const App = () => {
   return (
     <div className="app">
+      <Header />
       {/* <AuthForm /> */}
       <BookCard />
     </div>
