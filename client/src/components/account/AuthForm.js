c2fc69202400b1c90488010286b69c908d8ab1a3
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 14:29:24 -0700

    Complete tested backend /signup route

--- client/src/components/account/AuthForm.js
+++ client/src/components/account/AuthForm.js
@@ -13,10 +13,61 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { useState, useContext, useEffect } from "react";
 
+import { useFormik } from "formik";
+import * as yup from "yup";
+
 const defaultTheme = createTheme();
 
 const AuthForm = () => {
     const [ isLogin, setIsLogin ] = useState(false)
+
+    const userSchema = yup.object().shape({
+        username: yup
+        .string()
+        .min(5, "Username must be at least 5 characters")
+        .max(20, "Username must be at most 20 characters")
+        .test(
+            "valid-chs",
+            "Username may only contain letters and numbers",
+            (value) => {
+            return /^[A-z0-9]+$/.test(value);
+            }
+        )
+        .required("Username is required"),
+        password: yup
+        .string()
+        .min(10, "Password must be at least 10 characters")
+        .required("Password is required"),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: ""
+        },
+        validationSchema: userSchema,
+        onSubmit: (values) => {
+            fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(values)
+            })
+            .then(res => {
+                if (res.ok) {
+                    res.json()
+                    .then(data => {
+                        console.log(data)
+                    })
+                } else {
+                    res.json()
+                    .then(err => console.log(err.error))
+                }
+            })
+            .catch(err => console.log(err))
+        }
+    })
 
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -35,7 +86,11 @@ const AuthForm = () => {
                     <Typography component="h1" variant="h5">
                     {isLogin ? "Log in" : "Sign up"}
                     </Typography>
-                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                    <Box 
+                        component="form" 
+                        onSubmit={formik.handleSubmit} 
+                        noValidate sx={{ mt: 1 }}
+                        >
                     <TextField
                         margin="normal"
                         required
@@ -45,6 +100,8 @@ const AuthForm = () => {
                         name="username"
                         autoComplete="username"
                         autoFocus
+                        onChange={formik.handleChange}
+                        value={formik.values.username}
                     />
                     {isLogin ? null :
                         <TextField
@@ -55,6 +112,8 @@ const AuthForm = () => {
                             label="Email Address"
                             name="email"
                             autoComplete="email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
                             // autoFocus
                         />}
                     <TextField
@@ -66,6 +125,8 @@ const AuthForm = () => {
                         type="password"
                         id="password"
                         autoComplete="current-password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
                     />
                     <Button
                         type="submit"
