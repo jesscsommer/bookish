8a03879b3e2c9a6e3c3435b7fea94d5164ed88bb
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 11:40:32 -0700

    Connect books to basic cards

--- client/src/components/books/BooksContainer.js
+++ client/src/components/books/BooksContainer.js
@@ -1,20 +1,14 @@
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { useContext } from "react"
 import { BookContext } from '../../context/bookContext';
 
-const defaultTheme = createTheme()
+import BookCard from './BookCard';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const defaultTheme = createTheme()
 
 const BooksContainer = () => {
     const { books } = useContext(BookContext)
@@ -26,32 +20,7 @@ const BooksContainer = () => {
             <Container sx={{ py: 8 }} maxWidth="md">
                 <Grid container spacing={4}>
                 {books.map((book) => (
-                    <Grid item key={book.id} xs={12} sm={6} md={4}>
-                    <Card
-                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                    >
-                        <CardMedia
-                        component="div"
-                        sx={{
-                            // 16:9
-                            pt: '56.25%',
-                        }}
-                        image={book.cover_photo}
-                        />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {book.title}
-                        </Typography>
-                        <Typography>
-                            {book.description}
-                        </Typography>
-                        </CardContent>
-                        <CardActions>
-                        <Button size="small">View</Button>
-                        <Button size="small">Edit</Button>
-                        </CardActions>
-                    </Card>
-                    </Grid>
+                    <BookCard key={book.id} book={book} />
                 ))}
                 </Grid>
             </Container>
