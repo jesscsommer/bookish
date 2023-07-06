a6a0d8f111c07fac41b1d173f26bb0bc36d10d96
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 16:37:52 -0700

    Add logout logic

--- server/blueprints/auth/logout.py
+++ server/blueprints/auth/logout.py
@@ -8,11 +8,13 @@ from blueprints import (
     g
 )
 
-from config import app 
+from config import app, unset_jwt_cookies 
 from models import db
 
 logout_bp = Blueprint("logout", __name__)
 
 @logout_bp.route("/logout", methods=["DELETE"])
 def logout():
-    return make_response("Reached logout!", 200) 
+    res = make_response({}, 204)
+    unset_jwt_cookies(res)
+    return res
