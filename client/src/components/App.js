75fa340370266737ceff72769c62b84c6db14759
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 10:36:28 -0700

    Build client-side routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AddShelfForm from "./shelves/AddShelfForm";
 import AuthForm from "./account/AuthForm";
 import BookCard from "./books/BookCard";
@@ -13,25 +13,58 @@ import Error from "./building_blocks/Error";
 import Header from "./building_blocks/Header";
 import Profile from "./account/Profile";
 import Shelf from "./shelves/Shelf";
+import NotFound from "./building_blocks/NotFound";
 
 const App = () => {
-  return (
-    <div className="app">
-      <Header />
-      {/* <AuthForm /> */}
-      {/* <BookCard />
-      <EditButton />
-      <DeleteButton />
-      <Buttons /> 
-      <Error />
-      <EditProfileForm />
-      <AddShelfForm />
-      <Profile />
-      <BooksContainer />
-      <BookDetail />
-      <Shelf /> */}
-    </div>
-  )
+    return (
+        <div className="app">
+        <Header />
+        {/* <AuthForm /> */}
+        {/* <BookCard />
+        <EditButton />
+        <DeleteButton />
+        <Buttons /> 
+        <Error />
+        <EditProfileForm />
+        <AddShelfForm />
+        <Profile />
+        <BooksContainer />
+        <BookDetail />
+        <Shelf /> */}
+        <Routes>
+            <Route 
+            path="/login" 
+            element={
+                <AuthForm />
+            }/>
+            <Route 
+                path="/shelves" 
+                element={
+                    <Shelf />
+                }/>
+            <Route 
+                path="/profile/:username" 
+                element={
+                <Profile />
+            }/>
+            <Route 
+                path="/books/:id" 
+                element={
+                    <BookDetail />
+                }/>
+            <Route 
+                path="/" 
+                element={
+                    <BooksContainer />
+                }/>
+            <Route 
+                path="*" 
+                element={
+                    <NotFound />
+                }/>
+        </Routes>
+        </div>
+    )
 }
 
 export default App
