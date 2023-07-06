0d6f51ef10af12712bce0408db8888a4a07bb8eb
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-06 09:59:53 -0700

    Add form dialogs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import AddShelfForm from "./shelves/AddShelfForm";
 import AuthForm from "./account/AuthForm";
 import BookCard from "./books/BookCard";
 import Buttons from "./building_blocks/Buttons";
 import DeleteButton from "./building_blocks/DeleteButton";
 import EditButton from "./building_blocks/EditButton";
+import EditProfileForm from "./account/EditProfileForm";
 import Error from "./building_blocks/Error";
 import Header from "./building_blocks/Header";
 
@@ -18,6 +20,8 @@ const App = () => {
       <DeleteButton />
       <Buttons /> 
       <Error />
+      <EditProfileForm />
+      <AddShelfForm />
     </div>
   )
 }
