f90a1f5d20f6c9676fa0ed020001c7e8e2d3583b
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-14 09:48:15 -0700

    Add review schema

---
+++ server/schemas/review_schema.py
@@ -0,0 +1,50 @@
+## rating : number, int or float that ends in 0.5 only, can't be null
+## max length for the comment, can't be null
+## same user can't review the same book twice
+
+from schemas import (
+    fields,
+    ma,
+    validate,
+    validates, 
+    validates_schema,
+    Review,
+    ValidationError
+)
+
+from schemas.book_schema import BookSchema
+from schemas.user_schema import UserSchema
+
+from models.book import Book
+from models.user import User
+
+class ReviewSchema(ma.SQLAlchemySchema):
+    class Meta():
+        model = Review
+        load_instance = True
+        ordered = True
+        fields = ("id", "rating", "comment", "book_id", "user_id", "url")
+        
+    rating = fields.Number(required=True, \
+                    validate=validate.Range(min=0.5, max=5), \
+                    error="Rating must be between 0.5 and 5")
+    comment = fields.String(required=True, \
+                    validate=validate.Length(min=1, max=2000), \
+                    error="Comment must be less than 2000 characters")
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "reviewbyid",
+                values=dict(id="<id>")
+            ),
+            "collection": ma.URLFor("reviews")
+        }
+    )
+
+    @validates_schema
+    def validate_object(self, data, **kwargs):
+        if Review.query.filter(Review.book_id == data["book_id"]) \
+                            .filter(Review.user_id == data["user_id"]) \
+                            .first(): 
+            raise ValidationError("Book already reviewed")
