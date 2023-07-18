4e57a3e888153e9bfa3766fab13462e4b1c93448
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-07-18 12:24:33 -0700

    Update add to shelf context and conditions

--- client/src/components/shelves/AddToShelfForm.js
+++ client/src/components/shelves/AddToShelfForm.js
@@ -19,12 +19,14 @@ import Error from '../building_blocks/Error';
 import Cookies from "js-cookie"
 import { ShelfContext } from '../../context/shelfContext';
 import { BookContext } from '../../context/bookContext';
+import { BookShelfContext } from '../../context/bookShelfContext';
 
 const AddToShelfForm = ({ book }) => {
     const [open, setOpen] = useState(false);
     const { user, dispatch : userDispatch } = useContext(UserContext)
     const { shelves, dispatch : shelfDispatch } = useContext(ShelfContext)
     const { books, dispatch : bookDispatch } = useContext(BookContext)
+    const { bookShelves, dispatch: bookShelfDispatch } = useContext(BookShelfContext)
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -56,10 +58,11 @@ const AddToShelfForm = ({ book }) => {
                 })
                 if (res.ok) {
                     const data = await res.json()
-                    shelfDispatch({ type: "patch", payload: data.shelf })
-                    user.book_shelves.push(data)
-                    userDispatch({ type: "fetch", payload: user })
-                    bookDispatch({ type: "patch", payload: data.book })
+                    bookShelfDispatch({ type: "add", payload: data })
+                    // shelfDispatch({ type: "patch", payload: data.shelf })
+                    // user.book_shelves.push(data)
+                    // userDispatch({ type: "fetch", payload: user })
+                    // bookDispatch({ type: "patch", payload: data.book })
                     resetForm()
                 }
             })();
@@ -67,7 +70,7 @@ const AddToShelfForm = ({ book }) => {
     })
 
 
-    const alreadyInShelves = book?.shelves?.filter(s => s.user_id === user.id).map(s => s.id)
+    const alreadyInShelves = bookShelves.filter(bs => bs.book_id == book.id).map(bs => bs.shelf_id)
     const notInShelves = shelves?.filter(shelf => !alreadyInShelves?.includes(shelf.id))
 
     return (
